572db7b3f48eb094c4e40bdbd501ef741ad0fe32
Edwon <edwonia@gmail.com>
2015-04-13 22:45:38 -0700

    re-organizing, added new info tab

---
+++ XCode/Sweat Pet/Sweat Pet/InfoViewController.swift
@@ -0,0 +1,14 @@
+//
+//  InfoViewController.swift
+//  Sweat Pet
+//
+//  Created by Edwon Edwon on 4/13/15.
+//  Copyright (c) 2015 EDWON. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController
+{
+
+}
