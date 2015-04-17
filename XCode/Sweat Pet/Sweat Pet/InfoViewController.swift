eed83ebca5c5da7017c33b2760a606e3f88edc57
Edwon <edwonia@gmail.com>
2015-04-17 09:32:19 -0700

    got update loop working! the code is in InfoViewController

--- XCode/Sweat Pet/Sweat Pet/InfoViewController.swift
+++ XCode/Sweat Pet/Sweat Pet/InfoViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class InfoViewController: UIViewController
 {
+    
+    var timer: NSTimer?
+    
     let petControl = PetController()
     
     override func viewDidLoad()
@@ -17,6 +20,20 @@ class InfoViewController: UIViewController
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         petControl.updateAge()
+    }
+    
+    override func viewDidAppear(animated: Bool)
+    {
+        println("info viewDidAppear")
+            dispatch_async(dispatch_get_main_queue(),
+            {
+                self.startUIUpdateTimer()
+            })
+    }
+    
+    override func viewDidDisappear(animated: Bool)
+    {
+        timer?.invalidate()
     }
     
     override func didReceiveMemoryWarning()
@@ -28,6 +45,24 @@ class InfoViewController: UIViewController
     override func prefersStatusBarHidden() -> Bool
     {
         return true
+    }
+    
+    @IBOutlet weak var testLabel: UILabel!
+    
+    func startUIUpdateTimer() {
+        // NOTE: For our purposes, the timer must run on the main queue, so use GCD to make sure.
+        //       This can still be called from the main queue without a problem since we're using dispatch_async.
+        dispatch_async(dispatch_get_main_queue()) {
+            // Start a timer that calls self.updateUI() once every tenth of a second
+            self.timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target:self, selector:"updateUI", userInfo: nil, repeats: true)
+        }
+    }
+    
+    var poop = Int(0)
+    
+    func updateUI() {
+        poop += Int(1)
+        testLabel.text = "updating \(poop)"
     }
 
 }
