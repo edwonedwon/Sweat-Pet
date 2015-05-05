05b2ccfd097933fae6e20c4369d0daba6fac7434
Edwon <edwonia@gmail.com>
2015-05-05 14:13:40 -0700

    Health Manager notes for Arthur

--- XCode/Sweat Pet/SweatPetUtil/HealthManager.swift
+++ XCode/Sweat Pet/SweatPetUtil/HealthManager.swift
@@ -5,6 +5,72 @@
 ////  Created by Edwon Edwon on 5/2/15.
 ////  Copyright (c) 2015 EDWON. All rights reserved.
 
+// HEALTH MANAGER, a utility class that the Sweat Pet iPhone App and Watch Kit Extension can both use for getting health data
+
+/* NOTES FOR ARTHUR
+
+    Most of the current code is copied from http://www.raywenderlich.com/86336/ios-8-healthkit-swift-getting-started
+    I understand some of it, but I'm having trouble understanding the use of completions in the code
+    This is where Arthur comes in to save the day!
+
+    In the game, I want it so you get 1 vial of sweat for every 15 minutes of exercise.
+    Any exercise beyond that in one day starts to "supercharge" the sweat, which helps you level up your pet faster.
+
+    To help users know how much they've worked out, I also have a screen where you can see the vial filling up throughout the day or while exercising.
+
+    Exercise is not tracked using Sweat Pet, but rather any tracking app the user happens to use.
+    Using Apple's HealthKit, their health data will be stored in the Health app included on every iPhone
+    https://developer.apple.com/library/ios/documentation/HealthKit/Reference/HealthKit_Framework/
+
+    Now, the hard part is figuring out how to judge what is exercise and what is not from HealthKits data.
+    This get's a bit tricky as there are a few different ways exercise data is stored.
+
+    "Active Calories" - calories burned while exercising (not just hanging out)
+    "Heart Rate" - we could count anything over 100bpm as exercise 
+    "Distance" - how long somebody ran or rode their bike
+    "Workouts" - these are basically for apps like "Strava" and the built in Apple Watch "Workout" app, it's a collection of data related to one "workout" like a jog or a run the user specifically tracked, I can't tell whether data from Workouts is added to the main "heart rate" and "active calories" in the users health database https://developer.apple.com/library/ios/documentation/HealthKit/Reference/HKWorkout_Class/index.html#//apple_ref/doc/uid/TP40014744
+
+    What I need is basically some high level function
+    that returns the total amount of time the user has exercised on any given day, or range of days, using some sort of magic with this data to figure out whether the user was actually exercising or not.
+    I want it based on time because I heard a doctor talking about the minimum exercise needed to stay healthy. It's basically 15 minutes a day above a certain heart rate (I think 100bpm or so) and your health increases dramatically. So I want people to be motivated to simply do that.
+
+    So I'm thinking it should kinda look like this
+
+    var userPrefsDayBeginTime:NSTime
+
+    func getTodaysExercise (day:NSDate) -> NSTimeInterval
+    {
+        insert arthur magic
+        return totalTimeExercised
+    }
+
+    func getMultipleDaysExercise (startDate:NSDate, endDate:NSDate) -> NSTimeInterval
+    {
+        insert arthur magic
+        return totalTimeExercised
+    }
+
+    func exerciseTimeToMinutes (totalTimeExercise:NSTimeInterval) -> Int
+    {
+        insert arthur magic
+        return totalMinutes
+    }
+
+
+    I also plan on having a setting in the app where users can set when a "day" begins and ends. 
+    For instance if you want your day to begin and end at 2am because you often workout late at night. 
+    Then this function should be used to help calculate the totalTimeExercised in the other functions
+
+    func getUserDay ()
+    {
+        check against userPrefsDayBeginTime preference
+    }
+
+    At some point I'd also like to add a feature where users can record workouts inside the Sweat Pet app, but I imagine it's out of your scope for now.
+
+    Thanks so much for this Arthur, and please leave comments or take me through the code when your done as I'd like to learn how this stuff works!
+
+*/
 
 import Foundation
 import HealthKit
@@ -99,7 +165,7 @@ public class HealthManager
     
     
     // AUTHORIZE HEALTH KIT
-    // this should only need to happen once
+    // this crashes the app right now for some reason
     public func authorizeHealthKit(completion: ((success:Bool, error:NSError!) -> Void)!)
     {
         // 1. Set the types you want to read from HK Store
